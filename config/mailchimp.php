dfbfbcdfb56a947ca83eb686711c61e7ea341578
Guillaume <gui@nibelheimdesign.com>
2019-12-04 14:22:54 +0100

    config: bootstrap version

--- config/mailchimp.php
+++ config/mailchimp.php
@@ -1,8 +1,7 @@
 <?php
 
 return [
-    // Mailchimp API key format 12345678901234567890123456789012-xx1
     'api_key' => env('MAILCHIMP_API_KEY', null),
-
-    'list_id' => env('MAILCHIMP_LIST_ID', null)
+    'list_id' => env('MAILCHIMP_LIST_ID', null),
+    'bootstrap_version' => 4
 ];
